2541eb9c210ff889882e5a4a99ec6fd4c2944074
torrison <cd99@mail.ru>
2014-11-08 21:31:45 +0200

    0.1.31

--- php/hlkiller/view/footer.php
+++ php/hlkiller/view/footer.php
@@ -1,1 +1,47 @@
-<?php echo 'footer'; ?>
+<script type="text/javascript">
+  $(function(){
+  
+	$('.hl_btn').on('click', function() {
+		$('.generete_block').hide()
+		$('.high_load_block').show()
+		$('.hl_btn').addClass('btn-success')
+		$('.gen_btn').removeClass('btn-success')
+	});
+	$('.gen_btn').on('click', function() {
+		$('.generete_block').show()
+		$('.high_load_block').hide()
+		$('.gen_btn').addClass('btn-success')
+		$('.hl_btn').removeClass('btn-success')
+	});
+	
+	$('.gen_fish').on('click', function() {
+		
+			$.get('/hlkiller/index.php?do=generate_fish', function(data){
+			
+				$('#result_div').html(data);
+			
+			});
+	});
+	
+	$('.clear_db').on('click', function() {
+		
+			$.get('/hlkiller/index.php?do=clear_db', function(data){
+			
+				$('#result_div').html(data);
+			
+			});
+	});
+	
+	$('.max_select_attack').on('click', function() {
+	
+		$.get('/hlkiller/index.php?do=make_select_attack', function(data){
+			
+			$('#result_div').html(data);
+			
+		});
+	
+	});
+
+  
+  });
+</script>
--- php/hlkiller/view/index.php
+++ php/hlkiller/view/index.php
@@ -1,1 +1,37 @@
-<?php echo 'index'; ?>
+<div class="top_block">
+	<div class="container">
+		<div class="row top50">
+			<div class="span6">
+				<h1>High load killer</h1>
+				<div class="main_slogan white">Test your server for high loads<span class="bold ml20">NOW</span></div>
+			</div>
+			<div class="span6 right_side">
+				<div class="btn btn-large top_btn btn-success gen_btn">Generate</div>
+				<div class="btn btn-large top_btn hl_btn">High load</div>
+				</br></br>
+
+				<div class="btn btn-large input_btn gen_fish">Generate fish</div>
+				<div class="btn btn-large clear_db">Clear DataBase</div>
+	
+				<div class="high_load_block hidden">
+						<a class="btn btn-large high_load_btn max_select_attack">Max SELECT Attack #1</a>
+						<a class="btn btn-large high_load_btn">Max SELECT Attack #2</a>
+						</br></br>
+						<a class="btn btn-large high_load_btn">Mixed Attack #1</a>
+						<a class="btn btn-large high_load_btn">Mixed Attack #2</a>
+						</br></br>
+						<a class="btn btn-large high_load_btn">Middle load #1</a>
+						<a class="btn btn-large high_load_btn">Middle load #2</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="info_block">
+	<div class="well result_btn"><button class="btn btn-large btn-block btn btn-success" type="button">See result</button></div>
+	<div class="container">
+		<div class="main_slogan">Results:</div>
+		<div id="result_div" class="result_div">
+		</div>
+	</div>
+</div>
