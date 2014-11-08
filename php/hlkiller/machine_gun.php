f6dc13bd53d96c25c0441a6b637a436ce67d1aa3
Denis <denikewebpost@yandex.ua>
2014-11-08 20:36:18 +0200

    0.1.26

--- php/hlkiller/machine_gun.php
+++ php/hlkiller/machine_gun.php
@@ -10,12 +10,75 @@ class machine_gun {
 		';
 		$tablename = 'users';*/
 
+		$current_user_id = (int) '1';
+
 		$fields = '
-			`users_id`,
+			`posts_id`,
+			`posts_created_time`,
+			`posts_title`,
+			`posts_html`,
+
 			`users_username`,
-			`users_password`
+			`users_password`,
+
+			`cats_names_string`,
+			`cats_id_string`,
+
+			`last_comments_users_id`,
+			`last_comments_username`,
+
+			`likes_count`
 		';
+
+		$JOINs = "
+
+			LEFT JOIN `users` ON `users`.`users_id`=`posts`.`users_id`
+			LEFT JOIN (
+					SELECT
+						`posts_id`,
+						GROUP_CONCAT (`categories_name` ORDER BY `categories_name` ASC SEPARATOR '[;]') as `cats_names_string`,
+						GROUP_CONCAT (`categories_id`   ORDER BY `categories_name` ASC SEPARATOR '[;]') as `cats_id_string`
+					FROM
+						`categories`
+					LEFT JOIN
+						`rel_posts_categories` ON
+						`categories`.`categories_id`=`rel_posts_categories`.`categories_id`
+					GROUP BY
+						`posts_id`
+				) `posts_cats_strings` ON `posts_cats_strings`.`posts_id`=`posts`.`posts_id`
+			LEFT JOIN `rel_user_following` ON `rel_user_following`.`users_supplier_id`=`users`.`users_id`
+			LEFT JOIN (
+					SELECT
+						`posts_id`,
+						`users_id` as `last_comments_users_id`,
+						`users`.`users_username` as `last_comments_username`,
+						MAX (`comments_id`),
+						`comments_text`
+					FROM
+						`rel_users_posts_comments`
+					LEFT JOIN
+						`users` ON
+						`users`.`users_id`=`rel_users_posts_comments`.`users_id`
+					GROUP BY
+						`posts_id`
+				) `last_comments` ON `last_comments`.`posts_id`=`posts`.`posts_id`
+			LEFT JOIN (
+					SELECT
+						`posts_id`,
+						COUNT (`rel_users_posts_like_id`) as `likes_count`
+					FROM
+						`users_posts_like`
+					GROUP BY
+						`posts_id`
+				) `likes` ON `likes`.`posts_id`=`posts`.`posts_id`
+
+		";
 		$tablename = 'posts';
+		$conditions = "
+
+			`rel_user_following`.`users_supplier_id`='$current_user_id'
+
+		";
 
 		$query_text = "
 
@@ -23,8 +86,9 @@ class machine_gun {
 				$fields
 			FROM
 				`$tablename`
+			$JOINs
 			WHERE
-				1
+				$conditions
 
 		";
 		\hlkiller_core::db()->query ($query_text);
