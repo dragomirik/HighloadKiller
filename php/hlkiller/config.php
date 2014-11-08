615eb9d6023840119cddabdd5b554e06a34527ae
Viktor Vetsko <viktor.vetsko@gmail.com>
2014-11-08 17:55:19 +0200

    0.1.19

--- php/hlkiller/config.php
+++ php/hlkiller/config.php
@@ -6,4 +6,33 @@ class config {
 	public static $db_username   = 'root';
 	public static $db_userpass   = '';
 	public static $db_name       = 'killer_test';
+        public static $tables  = array(
+            'users'=>array(
+                'users_id'=> array(
+                    'type'=>'int',
+                    'primary'=>true
+                    
+                    ),
+                'user_name'=>array(
+                    'length'=>100,
+                    'type'=>'string'
+                ),
+                'users_password'=>array(
+                    'length'=>32,
+                    'type'=>'string' 
+                )
+            ),
+            'categories'=>array(
+                'categories_id'=>array(
+                    'type'=>'int',
+                    'primary'=>true
+                    
+                    ),
+                'categories_name'=>array(
+                    'length'=>100,
+                    'type'=>'string'
+                )
+            )
+            
+        );
 }
