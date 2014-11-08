0e5e176b1234eb5d71acd4963cf8f98316b688f9
Denis <denikewebpost@yandex.ua>
2014-11-08 18:28:59 +0200

    0.1.21.2

--- php/hlkiller/config.php
+++ php/hlkiller/config.php
@@ -2,7 +2,6 @@
 	class config {
 		const VIEW_PATH = 'hlkiller/view';
 
-<<<<<<< HEAD
 		public static $db_server     = '127.0.0.1';
 		public static $db_username   = 'root';
 		public static $db_userpass   = '';
@@ -122,38 +121,3 @@
 			)
 		);
 	}
-=======
-	public static $db_server     = '127.0.0.1';
-	public static $db_username   = 'root';
-	public static $db_userpass   = '';
-	public static $db_name       = 'killer_test';
-        public static $tables  = array(
-            'users'=>array(
-                'users_id'=> array(
-                    'type'=>'int',
-                    'primary'=>true
-                    ),
-                'user_name'=>array(
-                    'length'=>100,
-                    'type'=>'string'
-                ),
-                'users_password'=>array(
-                    'length'=>32,
-                    'type'=>'string' 
-                )
-            ),
-            'categories'=>array(
-                'categories_id'=>array(
-                    'type'=>'int',
-                    'primary'=>true
-                    
-                    ),
-                'categories_name'=>array(
-                    'length'=>100,
-                    'type'=>'string'
-                )
-            )
-            
-        );
-}
->>>>>>> origin/master
