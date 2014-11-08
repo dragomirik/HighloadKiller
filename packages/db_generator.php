bc563311d48a85fb50ad283de2bb9df697b9f60c
Denis <denikewebpost@yandex.ua>
2014-11-08 13:56:05 +0200

    0.1.1

---
+++ packages/db_generator.php
@@ -0,0 +1,6 @@
+<?php
+class db_generator {
+	public function init () {}
+	public function add_structurt_dump ($file_path) {}
+	public function generate_data () {}
+}
---
+++ packages/machine_gun.php
@@ -0,0 +1,4 @@
+<?php
+class machine_gun {
+
+}
