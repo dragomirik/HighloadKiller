a1fd90b0ef97d94077e11d3eb53c10410f846375
Denis <denikewebpost@yandex.ua>
2014-11-08 13:58:11 +0200

    0.1.2

--- packages/machine_gun.php
+++ packages/machine_gun.php
@@ -1,4 +1,8 @@
 <?php
 class machine_gun {
-
+	public function init () {}
+	public function make_select_attack () {}
+	public function make_mixed_attack () {}
+	public function high_load_emulation () {}
+	public function get_statistics () {}
 }
---
+++ packages/ui.php
@@ -0,0 +1,5 @@
+<?php
+class ui {
+	public function init () {}
+	public function get_ui () {}
+}
