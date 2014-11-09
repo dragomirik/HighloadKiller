61874121fa718f75ed6059631d37e8ac40edfe09
Denis <denikewebpost@yandex.ua>
2014-11-09 04:38:39 +0200

    0.3.2

--- php/hlkiller/machine_gun.php
+++ php/hlkiller/machine_gun.php
@@ -111,58 +111,13 @@ class machine_gun {
 			`posts_html`,
 
 			`users_username`,
-			`users_password`,
-
-			`cats_names_string`,
-			`cats_id_string`,
-
-			`last_comments_users_id`,
-			`last_comments_username`,
-
-			`likes_count`
+			`users_password`
 		';
 
 		$JOINs = "
 
 			LEFT JOIN `users` ON `users`.`users_id`=`posts`.`users_id`
-			LEFT JOIN (
-					SELECT
-						`rel_posts_categories`.`posts_id`,
-						GROUP_CONCAT(`categories_name` ORDER BY `categories_name` ASC SEPARATOR '[;]') as `cats_names_string`,
-						GROUP_CONCAT(`rel_posts_categories`.`categories_id`   ORDER BY `categories_name` ASC SEPARATOR '[;]') as `cats_id_string`
-					FROM
-						`categories`
-					LEFT JOIN
-						`rel_posts_categories` ON
-						`categories`.`categories_id`=`rel_posts_categories`.`categories_id`
-					GROUP BY
-						`rel_posts_categories`.`posts_id`
-				) `posts_cats_strings` ON `posts_cats_strings`.`posts_id`=`posts`.`posts_id`
 			LEFT JOIN `rel_users_following` ON `rel_users_following`.`users_supplier_id`=`users`.`users_id`
-			LEFT JOIN (
-					SELECT
-						`rel_users_posts_comments`.`posts_id`,
-						`users`.`users_id` as `last_comments_users_id`,
-						`users`.`users_username` as `last_comments_username`,
-						MAX(`rel_users_posts_comments_id`),
-						`comments_text`
-					FROM
-						`rel_users_posts_comments`
-					LEFT JOIN
-						`users` ON
-						`users`.`users_id`=`rel_users_posts_comments`.`users_id`
-					GROUP BY
-						`rel_users_posts_comments`.`posts_id`
-				) `last_comments` ON `last_comments`.`posts_id`=`posts`.`posts_id`
-			LEFT JOIN (
-					SELECT
-						`users_posts_like`.`posts_id`,
-						COUNT(`rel_users_posts_like_id`) as `likes_count`
-					FROM
-						`users_posts_like`
-					GROUP BY
-						`users_posts_like`.`posts_id`
-				) `likes` ON `likes`.`posts_id`=`posts`.`posts_id`
 
 		";
 		$tablename = 'posts';
@@ -180,9 +135,9 @@ class machine_gun {
 				`$tablename`
 			$JOINs
 			WHERE
-				$conditions
+				1
 			ORDER BY
-				`posts`.`posts_created_time` DESC
+				`posts`.`posts_created_time` ASC
 			LIMIT
 				0, $count
 		";
@@ -231,7 +186,7 @@ class machine_gun {
 	public function run_select ($query_text) {
 		//$times = (int) \testing_config::$select ['times'];
 		$times = $_GET ['times'];
-		$db = &\hlkiller_core::db ();
+		$db = \hlkiller_core::db ();
 		$start_time = microtime (2);
 		for ($i = 0; $i < $times; $i++){
 			try {
